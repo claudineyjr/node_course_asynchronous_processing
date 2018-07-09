3d149c45ad2592a9a4dad879f6b5f3fb6f746a93
Claudiney Junior <claudiney.alves@fatec.sp.gov.br>
2018-07-09 16:52:36 -0300

    Correção de identação

--- callback.js
+++ callback.js
@@ -1,8 +1,8 @@
 module.exports = function callbackExample(valueToHandle, callback) {
-    let error;
-    if (isNaN(valueToHandle)) {
-        error = 'Valor não é um número';
-    }
-    const result = valueToHandle * Math.PI;
-    callback(error, result, Math.PI);
+  let error;
+  if (isNaN(valueToHandle)) {
+    error = 'Valor não é um número';
+  }
+  const result = valueToHandle * Math.PI;
+  callback(error, result, Math.PI);
 }
--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const promiseExample = require('./promise');
 const numberToMultiplicate = 12;
 const stringToTestErrorHandle = 'Curso IBM Node.js + Angular';
 
-callbackExample(numberToMultiplicate, function(error, result, multiplicator) {
-    if (!error) {
-        console.log(`O número ${numberToMultiplicate} multiplicado por ${multiplicator} resulta em ${result}.`)
-    } else {
-        console.log(error);
-    }
+callbackExample(numberToMultiplicate, function (error, result, multiplicator) {
+  if (!error) {
+    console.log(`O número ${numberToMultiplicate} multiplicado por ${multiplicator} resulta em ${result}.`)
+  } else {
+    console.log(error);
+  }
 });
 
 return promiseExample(numberToMultiplicate)
-    .then((result) => console.log(result))
-    .catch((error) => console.log(error));
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
